ffc1f27e0325c5318943b76e1f3357cb67d048c6
Jonas Bo Jalling <jonas@jalling.dk>
2017-08-02 19:19:53 +0200

    Minor changes (prim. identation)

--- hardware/LKIHC.cpp
+++ hardware/LKIHC.cpp
@@ -76,31 +76,33 @@ void CLKIHC::Do_Work()
 
 
 
-
+    bool firstConnect = true;
     int sec_counter = 0;
 
     while (!m_stoprequested)
-
     {
-                if (ihcC->CONNECTED == ihcC->connState)
+        if (ihcC->CONNECTED == ihcC->connState)
         {
             std::vector<int> resourceIdList;
 
-            std::vector<std::vector<std::string> > result;
-            result = m_sql.safe_query("SELECT DeviceID FROM DeviceStatus WHERE HardwareID==%d AND Used == 1", m_HwdID);
-
-            if (result.size() > 0)
+            if (firstConnect)
             {
-                std::vector<std::vector<std::string> >::const_iterator itt;
-                for (itt = result.begin(); itt != result.end(); ++itt)
+                std::vector<std::vector<std::string> > result;
+                result = m_sql.safe_query("SELECT DeviceID FROM DeviceStatus WHERE HardwareID==%d AND Used == 1", m_HwdID);
+
+                if (result.size() > 0)
                 {
-                    std::vector<std::string> sd = *itt;
-                    resourceIdList.push_back(std::strtoul(sd[0].c_str(), NULL, 16));
+                    std::vector<std::vector<std::string> >::const_iterator itt;
+                    for (itt = result.begin(); itt != result.end(); ++itt)
+                    {
+                        std::vector<std::string> sd = *itt;
+                        resourceIdList.push_back(std::strtoul(sd[0].c_str(), NULL, 16));
+                    }
                 }
+
+                ihcC->enableRuntimeValueNotification(resourceIdList);
+                firstConnect = false;
             }
-
-            ihcC->enableRuntimeValueNotification(resourceIdList);
-
 
             std::vector<boost::shared_ptr<ResourceValue> > updatedResources;
             updatedResources = ihcC->waitResourceValueNotifications(RESOURCE_NOTIFICATION_TIMEOUT_S);
