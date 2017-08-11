ae025fd037f7607c2abee8af59cb7703670fac27
Jonas Bo Jalling <jonas@jalling.dk>
2017-08-11 09:32:27 +0200

    Add license

--- hardware/LKIHC.cpp
+++ hardware/LKIHC.cpp
@@ -1,3 +1,38 @@
+/*!
+ * \file hardware/LKIHC.cpp
+ * \author  Jonas Bo Jalling <jonas@jalling.dk>
+ * \version 0.1
+ *
+ * \section LICENSE
+ *
+ * The MIT License
+ *
+ * Copyright (c) 2017 Jonas Bo Jalling
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * \section DESCRIPTION
+ *
+ * This provides a interface to the LK IHC home automation system
+ *
+ */
+
 #include "stdafx.h"
 #include "LKIHC.h"
 #include "../main/Helper.h"
@@ -77,9 +112,10 @@ void CLKIHC::Do_Work()
     {
         ihcC->openConnection();
     }
-    catch (const char* msg)
+    catch (...)//const char* msg)
     {
-        _log.Log(LOG_ERROR, "LKIHC Plugin: Exception: '%s' connecting to '%s'", msg, m_IPAddress.c_str());
+        std::cout << "Fejl\n";
+        //_log.Log(LOG_ERROR, "LKIHC Plugin: Exception: '%s' connecting to '%s'", msg, m_IPAddress.c_str());
     }
 
     if (ihcC->CONNECTED == ihcC->connState)
